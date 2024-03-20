84c957af279558b941206269af4416d2301c8b6c
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-20 16:44:28 +0000

    Update chech.js

--- chech.js
+++ chech.js
@@ -1,4 +1,4 @@
-var names = ["sine", "poo", "moo", "login", "sineup", "jon", "jacson", "xander", "daniel", "adam", "sam", "harry", "nathan", "we", "ollieyt", "ollie", "jorden"];
+/*var names = ["sine", "poo", "moo", "login", "sineup", "jon", "jacson", "xander", "daniel", "adam", "sam", "harry", "nathan", "we", "ollieyt", "ollie", "jorden"];
 var go = "no";
 for (let index = 0; index < names.length; index++) {
     const element = window.sessionStorage.getItem(names[index]);
@@ -10,4 +10,4 @@ for (let index = 0; index < names.length; index++) {
 
 if(go != "yes"){
     window.location.href = "login.html"
-}
+}*\
