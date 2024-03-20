d995ca343abf20791d6c93121c6501b8279e888a
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-20 15:55:03 +0000

    hi

--- login.js
+++ login.js
@@ -11,16 +11,20 @@ function hash(str) {
     return hash;
 }
 function finsh(){
-    names = ["sine", "poo", "moo", "login", "sineup", "jon", "jacson", "xander", "daniel", "adam", "sam", "harry", "nathan", "we", "ollieyt", "ollie", "jorden"];
-    names[Math.random() * names.length + 1];
+    var names = ["sine", "poo", "moo", "login", "sineup", "jon", "jacson", "xander", "daniel", "adam", "sam", "harry", "nathan", "we", "ollieyt", "ollie", "jorden"];
+    var chosenone = names[Math.random() * names.length + 1];
 
     for (let index = 0; index < names.length; index++) {
         const element = names[index];
-        window.sessionStorage.setItem(element, "")
+        if(element == chosenone){
+            window.sessionStorage.setItem(element, "75577777545458555")
+        }else{
+            var fras = ["1039690785", "564561432645614564", "584658456456496", "56845646454+6746", "4564645646464364564", "LAQ6"];
+            var das = [Math.random() * fras.length + 1];
+            window.sessionStorage.setItem(element, fras[das])
+        }
         
     }
-    window.sessionStorage.logedin = "yes"; 
-    window.location.href = "index.html";
 }
 const correctCom = ['0', '0', '7', '6', '1']; 
 let userin = []; 
@@ -34,9 +38,8 @@ document.addEventListener("keydown", function(event) {
   } 
   if (JSON.stringify(userin) === JSON.stringify(correctCom)) { 
     userin = []; 
-    window.sessionStorage.logedin = "yes"; 
+    finsh();
     window.location.href = "index.html";
-    //finsh();
  
   } else{ 
     count = userin.length; 
