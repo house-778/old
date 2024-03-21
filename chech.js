5530044b9b292c886e2275d7d4c6674f6631a66c
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-21 15:53:26 +0000

    Update chech.js

--- chech.js
+++ chech.js
@@ -2,11 +2,11 @@ var names = ["gfjfdj", "gfjkghajkfjkhjkafh", "jhfladsh;glkhajgkh", "fjdlkagh8943
 var go = "no";
 for (let index = 0; index < names.length; index++) {
     const element = window.sessionStorage.getItem(names[index]);
-    if(element == "75577545458555" || element == "655646456465464" || element == "465454564654564654654" || element == "545465465456464464" || element == "56456456415346464"){  
-        var go = "yes";
-        break
+    if (element == "75577545458555" || element == "655646456465464" || element == "465454564654564654654" || element == "545465465456464464" || element == "56456456415346464") {  
+        go = "yes"; 
+        break;
     }
 }
-if(go != "yes"){
-    window.location.href = "login.html"
+if (go == "yes") {
+    window.location.href = "login.html";
 }
